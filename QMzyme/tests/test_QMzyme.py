47a774657925b234d153e6fe0524504e32b70273
Heidi Klem <45046493+hklem@users.noreply.github.com>
2023-09-09 20:14:16 -0600

    Update test_QMzyme.py

--- QMzyme/tests/test_QMzyme.py
+++ QMzyme/tests/test_QMzyme.py
@@ -3,13 +3,13 @@ Unit and regression test for the QMzyme package.
 """
 
 # Import package, test suite, and other packages as needed
-import sys
+#import sys
 
-import pytest
+#import pytest
 
-import QMzyme
+#import QMzyme
 
 
-def test_QMzyme_imported():
-    """Sample test, will always pass so long as import statement worked."""
-    assert "QMzyme" in sys.modules
+#def test_QMzyme_imported():
+    #"""Sample test, will always pass so long as import statement worked."""
+    #assert "QMzyme" in sys.modules
