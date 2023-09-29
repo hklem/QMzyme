72277694ef921699397819d1af47f1ecd633a57f
Heidi Klem <45046493+hklem@users.noreply.github.com>
2023-09-29 14:45:29 -0600

    Update test_QMzyme.py

--- QMzyme/tests/test_QMzyme.py
+++ QMzyme/tests/test_QMzyme.py
@@ -8,12 +8,44 @@ Unit and regression test for the QMzyme package.
 import sys
 import pytest
 import QMzyme
+from rdkit import Chem
+import os
 
+path = os.path.join('etc')
+
+def grab_test_data(test_file):
+    with try open(test_file, 'r') as f:
+        data=f.readlines()
+        raise Error("Test file {test_file} not found.")
+    residue_sequence 
+    number_of_residues
+    atom_sequence
+    number_of_atoms
+    atom_coords
+    resid_order
 
 def test_QMzyme_imported():
-    """Sample test, will always pass so long as import statement worked."""
+    """
+    Sample test, will always pass so long as import statement worked.
+    """
     assert "QMzyme" in sys.modules
 
-#def test_truncation_schemes():
-	#'''tbd'''
+def test_catalytic_center(test_file):
+    """
+    tbd
+    """
+    test_file = os.path.join(path,test_file)
+    with try open(test_file,'r'):
+        print('It worked')
+        raise Error('Test file {test_file} was not found.')
+
+def test_active_site(test_file='test_catalytic_center_chainA_DNX_202_active_site_distance_cutoff_4.pdb'):
+    """
+    tbd
+    """
+
+def test_truncation(test_file='test_catalytic_center_chainA_DNX_202_truncated_active_site_distance_cutoff_4.pdb'):
+    """
+    tbd
+    """
 
