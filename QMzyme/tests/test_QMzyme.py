4c1652e8c20d6a1b0861f24fe9e6bf93a7ab78ef
Heidi Klem <heidi.klem@nist.gov>
2023-10-06 09:38:33 -0600

    Cleaned up tests.

--- QMzyme/tests/test_QMzyme.py
+++ QMzyme/tests/test_QMzyme.py
@@ -9,7 +9,7 @@ import sys
 import pytest
 import QMzyme
 import os
-from QMzyme.protein_parser import collect_pdb_data
+from QMzyme.protein_parser import collect_pdb_data, collect_coords
 
 path = os.path.join(os.getcwd(),'etc')
 test_start_file='test.pdb'
@@ -20,20 +20,29 @@ model.catalytic_center(res_name='DNX',res_number=202,chain='A',
 model.active_site(distance_cutoff=4,save_file=True,output_file='test_active_site.pdb')
 model.truncate(save_file=True, output_file='test_truncate.pdb')
 
-def catalytic_center_test(test_end_file='test_end_catalytic_center_chainA_DNX_202.pdb'):
-    static_data = collect_pdb_data(os.path.join(path,test_end_file))
-    test_data = collect_pdb_data(data=model.catalytic_center_pdb)
-    return [(test_data[key],static_data[key],key) for key in test_data.keys()]
+catalytic_center_inp = model.catalytic_center_pdb
+active_site_inp = model.active_site_pdb
+truncated_active_site_inp = model.truncated_active_site_pdb
 
-def active_site_test(test_end_file='test_end_catalytic_center_chainA_DNX_202_active_site_distance_cutoff_4.pdb'):
-    static_data = collect_pdb_data(os.path.join(path,test_end_file))
-    test_data = collect_pdb_data(data=model.active_site_pdb)
-    return [(test_data[key],static_data[key],key) for key in test_data.keys()]
+catalytic_center_exp = 'test_end_catalytic_center_chainA_DNX_202.pdb'
+active_site_exp = 'test_end_catalytic_center_chainA_DNX_202_active_site_distance_cutoff_4.pdb'
+truncated_active_site_exp = 'test_end_catalytic_center_chainA_DNX_202_truncated_active_site_distance_cutoff_4.pdb'
 
-def truncate_test(test_end_file='test_end_catalytic_center_chainA_DNX_202_truncated_active_site_distance_cutoff_4.pdb'):
-    static_data = collect_pdb_data(os.path.join(path,test_end_file))
-    test_data = collect_pdb_data(data=model.truncated_active_site_pdb)
-    return [(test_data[key],static_data[key],key) for key in test_data.keys()]
+def collect_test_data(test_inp,test_exp):
+    data_exp = collect_pdb_data(file=os.path.join(path,test_exp),coords=False)
+    data_inp = collect_pdb_data(data=test_inp,coords=False)
+    #coord_diff = coord_diff_test(data_inp, data_exp)
+    return [(data_inp[key],data_exp[key],key) for key in data_exp.keys()]
+
+#def coord_diff_test(inp,exp):
+#    a = collect_coords(data=exp)
+#    b = collect_coords(data=inp)
+#    c = []
+#    for i in range(len(a)):
+#        if 
+#        c.append([a[i][j]-b[i][j] for j in range(3)])
+#    return c
+        
 
 def test_QMzyme_imported():
     """
@@ -41,15 +50,15 @@ def test_QMzyme_imported():
     """
     assert "QMzyme" in sys.modules
 
-@pytest.mark.parametrize('inp, expected, key',catalytic_center_test())
+@pytest.mark.parametrize('inp, expected, key',collect_test_data(catalytic_center_inp,catalytic_center_exp))
 def test_catalytic_center(inp,expected,key):
     assert inp == expected, "a change was detected in {}.".format(key)
     
-@pytest.mark.parametrize('inp, expected, key',active_site_test())
+@pytest.mark.parametrize('inp, expected, key',collect_test_data(active_site_inp,active_site_exp))
 def test_active_site(inp,expected,key):
     assert inp == expected, "a change was detected in {}.".format(key)
 
-@pytest.mark.parametrize('inp, expected, key',truncate_test())
+@pytest.mark.parametrize('inp, expected, key',collect_test_data(truncated_active_site_inp,truncated_active_site_exp))
 def test_truncate(inp,expected,key):
     assert inp == expected, "a change was detected in {}.".format(key)
 
