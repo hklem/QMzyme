f720d216dbedc1b71c7e26cb7e04283cfaf143b8
Heidi Klem <heidi.klem@nist.gov>
2024-01-25 15:47:44 -0700

    created atom_selection function

--- QMzyme/mdanalysis_wrapper.py
+++ QMzyme/mdanalysis_wrapper.py
@@ -1,1 +1,67 @@
-################################################################################ Code written by Heidi Klem while at# Colorado State University as a graduate student# in the Paton and McCullagh groups and at the# National Institute of Standards and Technology# as an NRC Postdoc (Fed).# e: heidiklem@yahoo.com or heidi.klem@nist.gov###############################################################################import MDAnalysis as mdadef res_selection(pdb=None, sel=''):    '''    Parameters    ----------    pdb : str, required        Filename of structure. The default is None.    sel : str, required        Selection command used by MDAnalysis. The default is ''.    Returns    -------    residues : list of dictionaries        Dictionary containing the residue name, residue number, and chain.    '''    u = mda.Universe(pdb)    if 'chain' in sel:        sel = sel.split('chain')        sel = sel[0]+' segid '+sel[1]    sel = u.select_atoms(sel)    res = [x for x in sel.residues]    for r in res:        res_dict = {        'Residue name': r.resname,        'Residue number': r.resnum,        'Chain': r.segid        }    return res_dict
+###############################################################################
+# Code written by Heidi Klem while at
+# Colorado State University as a graduate student
+# in the Paton and McCullagh groups and at the
+# National Institute of Standards and Technology
+# as an NRC Postdoc (Fed).
+# e: heidiklem@yahoo.com or heidi.klem@nist.gov
+###############################################################################
+
+import MDAnalysis as mda
+
+def res_selection(pdb=None, sel=''):
+    '''
+    Parameters
+    ----------
+    pdb : str, required
+        Filename of structure. The default is None.
+    sel : str, required
+        Selection command used by MDAnalysis. The default is ''.
+
+    Returns
+    -------
+    res_dict : list of dictionaries
+        Dictionary containing the residue name, residue number, and chain.
+
+    '''
+    u = mda.Universe(pdb)
+    if 'chain' in sel:
+        sel = sel.split('chain')
+        sel = sel[0]+' segid '+sel[1]
+    sel = u.select_atoms(sel)
+    res = [x for x in sel.residues]
+    res_list = []
+    for r in res:
+        res_dict = {
+        'Residue name': r.resname,
+        'Residue number': r.resnum,
+        'Chain': r.segid
+        }
+        res_list.append(res_dict)
+    # just added this right now because if res_list has multiple residues this will not work in the current form of generate.py... so that would need to be fixed first.
+    if len(res_list) == 1:
+        res_list = res_dict
+    return res_list
+
+def atom_selection(pdb=None, sel=''):
+    '''
+    Parameters
+    ----------
+    pdb : str, required
+        Filename of structure. The default is None.
+    sel : str, required
+        Selection command used by MDAnalysis. The default is ''.
+
+    Returns
+    -------
+    idx : list of atom indices (zero indexed).
+
+    '''
+    u = mda.Universe(pdb)
+    if 'chain' in sel:
+        sel = sel.split('chain')
+        sel = sel[0]+' segid '+sel[1]
+    sel = u.select_atoms(sel)
+    idx = [x-1 for x in sel.ids]
+
+    return idx
