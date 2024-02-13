e5f2bc9a786c654c075aa4dbf07286d0465e30d6
Heidi Klem <heidi.klem@nist.gov>
2024-02-13 14:45:15 -0700

    Removed old files

--- tests/etc/generate_test_data.py
+++
@@ -1,7 +0,0 @@
-import QMzyme
-
-model = QMzyme.generate_model(protein_file = 'test_end.pdb')
-model.catalytic_center(res_name='DNX',res_number=202,chain='A')
-model.active_site(distance_cutoff=4)
-model.truncate()
-
