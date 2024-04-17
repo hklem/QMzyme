8ff6889d4bb4af750d910c3726538f445fc680ab
Heidi Klem <heidi.klem@nist.gov>
2024-04-17 14:26:50 -0600

    removed unecessary setup

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import Extension, setup
-
-setup(
-    ext_modules=[
-        Extension(
-            name="QMzyme.Biopython.kdtrees", ["QMzyme/Biopython/kdtrees.c"]
-        )
-    ]
-)
