661f541e617d9ed63aed1e0b7dbe0dc27815f501
Heidi Klem <heidi.klem@nist.gov>
2024-05-08 08:39:50 -0600

    made compatible with redesiged RegionBuilder class

--- QMzyme/truncation_schemes.py
+++ QMzyme/truncation_schemes.py
@@ -10,7 +10,7 @@ truncation_schemes = {
 }
 
 
-#Fix bug: code currently overwrites the QMzyme region.
+# init atoms to new region directly? Currently the atom names for cap atoms are not ensured to be unique.
 def CA_terminal(region): 
     # u = region.get_AtomGroup().universe
     # residues = [MDAnalysisWrapper.get_parallel_residue(res, u) for res in region.get_AtomGroup().residues]
@@ -18,8 +18,9 @@ def CA_terminal(region):
     add_atoms = []
     cap_bond = [] # list of tuples of atoms. first atom in tuple is where cap goes, second atom is reference pt for bond length calc
     
-    #need to make sure region is in increasing resid order?
+    region_builder = RegionBuilder(f'{region.name}_truncated')
 
+    #need to make sure region is in increasing resid order?
     for res in region.residues:
         resname = res.resname
         if resname not in protein_residues:
@@ -35,29 +36,33 @@ def CA_terminal(region):
         if resname != 'PRO':
             Hatom = res.get_atom('H')
 
-        if preceding_Catom.id not in region.ids:
+        if preceding_Catom is not None and preceding_Catom.id not in region.ids:
             if resname != 'PRO':
-                add_atoms.append(cap_H(Natom, CAatom))
+                #add_atoms.append(cap_H(Natom, CAatom))
+                region_builder.init_atom(cap_H(Natom, CAatom))
                 remove_atoms.append(Natom)
                 remove_atoms.append(Hatom)
             if resname == 'PRO':
-                add_atoms.append(cap_H(preceding_Catom, Natom))
+                #add_atoms.append(cap_H(preceding_Catom, Natom))
+                region_builder.init_atom(cap_H(preceding_Catom, Natom))
 
-        if following_Natom.id not in region.ids:
-            add_atoms.append(cap_H(Catom, CAatom))
+        if following_Natom is not None and following_Natom.id not in region.ids:
+            #add_atoms.append(cap_H(Catom, CAatom))
+            region_builder.init_atom(cap_H(Catom, CAatom))
             remove_atoms.append(Catom)
             remove_atoms.append(Oatom)
-        
-    new_region = RegionBuilder(f'{region.name}_truncated')
+
     for atom in region.atoms:
         if atom not in remove_atoms:
-            new_region.init_atom(atom)
-    for atom in add_atoms:
-        new_region.init_atom(atom)
-    new_region.region.sort(key='id', in_place=True)
-    new_region.region.sort(key='resid', in_place=True)
+            #new_region.init_atom(atom)
+            region_builder.init_atom(atom)
+    # for atom in add_atoms:
+    #     new_region.init_atom(atom)
+    truncated_region = region_builder.get_region()
+    truncated_region.sort(key='id', in_place=True)
+    truncated_region.sort(key='resid', in_place=True)
 
-    return new_region.region
+    return truncated_region
     
 
 def CA_all(region):
