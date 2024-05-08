de381592ab5ec94c97d05032503f3425f3e1d451
Heidi Klem <heidi.klem@nist.gov>
2024-05-08 08:33:17 -0600

    modified GenerateModel to be ModelBuilder class to eliminate redundancy

--- QMzyme/GenerateModel.py
+++ QMzyme/GenerateModel.py
@@ -1,4 +1,4 @@
-from QMzyme.ModelBuilder import ModelBuilder
+from QMzyme.RegionBuilder import RegionBuilder
 from QMzyme.QMzymeModel import QMzymeModel
 import os
 import QMzyme.MDAnalysisWrapper as MDAwrapper
@@ -11,22 +11,35 @@ class GenerateModel(QMzymeModel):
         if name is None:
             name = os.path.basename(file).split('.')[0]
         universe = MDAwrapper.init_universe(file)
-        #model = ModelBuilder(model_name, universe).get_model()
-        self.__model_builder = ModelBuilder()
-        model = self.__model_builder.init_model(name, universe)
+        model = QMzymeModel(name, universe)
         self.__dict__.update(model.__dict__)
+
+
+    def __repr__(self):
+        return f"<ModelBuilder: Current QMzymeModel built from {self.starting_structure} contains {self.n_regions} region(s)>"
+
 
     def set_catalytic_center(self, selection):
         self.set_region('catalytic_center', selection)
         return self.regions[-1]
-    
+
+
     def set_region(self, region_name, selection):
         selection = translate_selection(selection, self.starting_structure)
-        self.__model_builder.init_region(region_name, selection)
-        #self.regions.append(selection)
-        # self.init_region(region_name, selection)
+        region_builder = RegionBuilder(region_name)
+        #region = region_builder.init_atom_group(selection).get_region()
+        region_builder.init_atom_group(selection)
+        region = region_builder.get_region()
+        self.add_region(region)
         return self.regions[-1]
     
+
     def truncate_region(self, region, truncation_scheme='CA_terminal'):
         new_region = truncation_schemes[truncation_scheme](region)
         return new_region
+    
+
+    def remove_region(self, region_index):
+        del self.regions[region_index]
+
+
