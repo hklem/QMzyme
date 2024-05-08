c85cc1b1e9fed1b246e86c97c6be099e92d8016c
Heidi Klem <heidi.klem@nist.gov>
2024-05-08 08:40:58 -0600

    made get_preceding_Catom and get_following_Natom compatible with very first and last residue of sequence

--- QMzyme/truncation_utils.py
+++ QMzyme/truncation_utils.py
@@ -9,16 +9,13 @@ def cap_H(replace_atom, fixed_atom, bond_length=1.09):
     :param fixed_atom: The Atom bound to replace_atom that serves as reference point for bond vector calculation.
     """
     new_position = set_bond_length(replace_atom.position, fixed_atom.position, bond_length)
-    #new_name = name_cap_H(fixed_atom.region, fixed_atom.resid)
     new_atom_dict = {
             'element': 'H',
             'type': 'H',  
-            #'name': new_name, 
             'name': 'H1',
             'position': new_position, 
             'mass': 1.00794,
         }
-    #new_atom = alter_atom(replace_atom, new_atom_dict)
     new_atom = create_new_atom(fixed_atom, new_atom_dict) # used fixed atom because sometimes replaced atom comes from a different residue
     return new_atom
 
@@ -40,6 +37,8 @@ def set_bond_length(mobile_coords, fixed_coords, new_length):
 
 def create_new_atom(atom, new_atom_dict):
     for key, val in atom.__dict__.items():
+        if key.startswith('_QMzymeAtom__'):
+            key = key.split('_QMzymeAtom__')[-1]
         if key not in new_atom_dict:
             new_atom_dict[key] = val
     new_atom = QMzymeAtom(**new_atom_dict)
@@ -48,13 +47,19 @@ def create_new_atom(atom, new_atom_dict):
 
 def get_preceding_Catom(region, resid):
     mda_atom = region.get_atom_group().universe.select_atoms(f'resid {resid-1} and name C')
-    atom = RegionBuilder('temp', mda_atom).get_region().atoms[0]
+    try:
+        atom = RegionBuilder('temp', mda_atom).get_region().atoms[0]
+    except:
+        atom = None
     return atom
 
 
 def get_following_Natom(region, resid):
     mda_atom = region.get_atom_group().universe.select_atoms(f'resid {resid+1} and name N')
-    atom = RegionBuilder('temp', mda_atom).get_region().atoms[0]
+    try:
+        atom = RegionBuilder('temp', mda_atom).get_region().atoms[0]
+    except:
+        None
     return atom
 
 
@@ -64,11 +69,3 @@ def has_Nterm_neighbor(atom):
 
 def has_Cterm_neighbor(atom):
     return atom.resid+1 in atom.region.resids
-
-
-
-# def alter_atom(atom, new_atom_dict):
-#     for key, val in new_atom_dict.items():
-#         if hasattr(atom, key):
-#             setattr(atom, key, val)
-#     return atom
