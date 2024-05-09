4f1fedb9afd2e379086f06ebed5e29fe4d93560e
Heidi Klem <heidi.klem@nist.gov>
2024-05-09 15:39:10 -0600

    added indices property (idx)

--- QMzyme/QMzymeRegion.py
+++ QMzyme/QMzymeRegion.py
@@ -7,6 +7,7 @@ import numpy as np
 from typing import TYPE_CHECKING, Any, Dict, Generic, List, Optional, TypeVar
 from QMzyme.QMzymeAtom import QMzymeAtom
 from MDAnalysis.core.groups import AtomGroup
+import warnings
 from QMzyme import MDAnalysisWrapper as MDAwrapper
 
 _QMzymeAtom = TypeVar("_QMzymeAtom", bound="QMzymeAtom")
@@ -37,7 +38,19 @@ class QMzymeRegion:
         
     @property
     def ids(self):
+        """
+        Returns a list of the atom numbers/ids from the original starting structure. An atom id
+        of an atom from the starting structure should not change. See idxs as an alternative.
+        """
         return [atom.id for atom in self.atoms]
+    
+    @property
+    def idxs(self):
+        """
+        Returns a list of the atom indices starting from 0. If the order of atoms changes the idx 
+        assigned to an atom will change. See ids as an alternative.
+        """
+        return [idx for idx in range(self.n_atoms)]
     
     @property
     def resids(self):
@@ -122,13 +135,14 @@ class QMzymeRegion:
             if res.resid == resid:
                 return res
 
-
     def write(self, filename=None):
+        warnings.filterwarnings('ignore')
         # Housekeeping
         if filename is None:
             filename = f"{'_'.join(self.name.split(' '))}.pdb"
         ag = self.convert_to_AtomGroup()
         ag.write(filename)
+        return filename
 
     def convert_to_AtomGroup(self):
         return MDAwrapper.build_universe_from_QMzymeRegion(self)
@@ -137,6 +151,39 @@ class QMzymeRegion:
         for id in ids:
             atom = self.get_atom(id)
             atom.set_fixed()
+
+    def get_ids(self, attribute: str, value):
+        """
+        Example: get_ids(attribute='type', value='CA')
+        """
+        ids = []
+        for atom in self.atoms:
+            if getattr(atom, attribute) == value:
+                ids.append(atom.id)
+        return ids
+    
+    def get_indices(self, attribute: str, value):
+        ids = self.get_ids(attribute, value)
+        return self.get_idxs_from_ids(ids)
+
+    def get_idxs_from_ids(self, ids):
+        """
+        Example: get_ids(attribute='type', value='CA')
+        """
+        idxs = []
+        for idx, atom in enumerate(self.atoms):
+            if atom.id in ids:
+                idxs.append(idx)
+        return idxs
+    
+    def check_missing_attr(self, attr):
+        missing = []
+        for atom in self.atoms:
+            if not hasattr(atom, attr) or getattr(atom, attr) == None:
+                missing.append(atom)
+        if missing != []:
+            raise UserWarning(f"The following atoms are missing {attr} information: {missing}")
+        
 
 
 class QMzymeResidue(QMzymeRegion):
