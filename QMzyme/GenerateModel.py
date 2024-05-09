bd0fec45bd613716bdc944340252954836cc31ac
Heidi Klem <heidi.klem@nist.gov>
2024-05-09 15:38:05 -0600

    enabled topology and trajectory support

--- QMzyme/GenerateModel.py
+++ QMzyme/GenerateModel.py
@@ -7,10 +7,12 @@ from QMzyme.truncation_schemes import truncation_schemes
 
 class GenerateModel(QMzymeModel):
     "The Director, building a complex representation."
-    def __init__(self, file, name=None):
+    def __init__(self, *args, name=None, universe=None, **kwargs):
+        if universe is None:
+            universe = MDAwrapper.init_universe(*args, **kwargs)
+        self.universe = universe
         if name is None:
-            name = os.path.basename(file).split('.')[0]
-        universe = MDAwrapper.init_universe(file)
+            name = os.path.basename(self.universe.filename).split('.')[0]
         model = QMzymeModel(name, universe)
         self.__dict__.update(model.__dict__)
 
@@ -24,8 +26,8 @@ class GenerateModel(QMzymeModel):
         return self.regions[-1]
 
 
-    def set_region(self, region_name, selection):
-        selection = translate_selection(selection, self.starting_structure)
+    def set_region(self, region_name='no_name', selection=None):
+        selection = translate_selection(selection, self.universe)
         region_builder = RegionBuilder(region_name)
         #region = region_builder.init_atom_group(selection).get_region()
         region_builder.init_atom_group(selection)
@@ -36,10 +38,13 @@ class GenerateModel(QMzymeModel):
 
     def truncate_region(self, region, truncation_scheme='CA_terminal'):
         new_region = truncation_schemes[truncation_scheme](region)
+        self.add_region(new_region)
         return new_region
-    
 
     def remove_region(self, region_index):
         del self.regions[region_index]
+
+    # def add_region(self, region):
+    #     self.regions.append(region)
 
 
