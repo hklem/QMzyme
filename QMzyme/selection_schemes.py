482d9eda1c8c6cac4c4ac5c1e48d9a44424d66cb
Heidi Klem <heidi.klem@nist.gov>
2024-05-14 08:06:03 -0600

    fixed distance_cutoff

--- QMzyme/selection_schemes.py
+++ QMzyme/selection_schemes.py
@@ -8,10 +8,13 @@ Module containing functions to define a QMzymeRegion based on some logic/workflo
 """
 
 import QMzyme.MDAnalysisWrapper as MDAwrapper
-from QMzyme.QMzymeRegion import QMzymeRegion
 from QMzyme.RegionBuilder import RegionBuilder
 
-def distance_cutoff(model, distance_cutoff, include_whole_residues=True):
+selection_schemes = {
+    'distance_cutoff': lambda model, cutoff: distance_cutoff(model, cutoff),
+}
+
+def distance_cutoff(model, cutoff, include_whole_residues=True):
     """
     Function to select QMzymeRegion based on ``distance_cutoff``. Ad hoc approach
     that is often used to create multiple models that will be evaluated for 
@@ -20,16 +23,20 @@ def distance_cutoff(model, distance_cutoff, include_whole_residues=True):
     if not model.has_region('catalytic_center'):
         raise UserWarning("You must first define a catalytic_center. See method `set_catalytic_center()`.")
     
+
+
     neighbors = MDAwrapper.get_neighbors(
-        model.starting_structure.select_atoms('all'),
-        model.get_region('catalytic_center').get_AtomGroup(),
-        distance_cutoff)
+        model.universe.select_atoms('all'),
+        model.get_region('catalytic_center').get_atom_group(),
+        cutoff)
     
     neighbors_byres = neighbors.residues.sorted_unique.atoms
 
+    #neighbors = model.universe.select_atoms(f"around {distance_cutoff}")
+
     # Convert MDAnalysis AtomGroup to QMzymeRegion
-    qmz_neighbors = RegionBuilder(name=f"Atoms_within_cutoff_{distance_cutoff}", atom_group=neighbors).get_region()
-    qmz_neighbors_byres = RegionBuilder(name=f"Residues_within_cutoff_{distance_cutoff}", atom_group=neighbors_byres).get_region()
+    qmz_neighbors = RegionBuilder(name=f"cutoff_{cutoff}", atom_group=neighbors).get_region()
+    qmz_neighbors_byres = RegionBuilder(name=f"cutoff_{cutoff}", atom_group=neighbors_byres).get_region()
 
     for atom in qmz_neighbors_byres.atoms:
         if atom.id in qmz_neighbors.ids:
