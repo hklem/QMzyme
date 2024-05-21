23484cb587d3c9a4c6a76c92eddb7248f3c83d5c
Heidi Klem <45046493+hklem@users.noreply.github.com>
2024-05-21 14:36:19 -0600

    Update __init__.py

--- QMzyme/__init__.py
+++ QMzyme/__init__.py
@@ -1,6 +1,6 @@
 """QM-based enzyme model generation and validation."""
 
 # Add imports here
-from QMzyme import *
-from QMzyme.GenerateModel import GenerateModel
-from QMzyme.CalculateModel import *
+#from QMzyme import *
+from .GenerateModel import GenerateModel
+from .CalculateModel import *
