0a2bc4d5784d93264c7e15f94a7121e7b0de98e4
Heidi Klem <heidi.klem@nist.gov>
2024-05-23 12:41:49 -0600

    added more data files

--- QMzyme/data/__init__.py
+++ QMzyme/data/__init__.py
@@ -5,6 +5,8 @@ _data_ref = resources.files('QMzyme.data')
 PDB = (_data_ref / '1oh0.pdb').as_posix()
 TOP = (_data_ref / '1oh0_equ.prmtop').as_posix()
 RST = (_data_ref / '1oh0_equ.rst7').as_posix()
+DCD = (_data_ref / '1oh0_equ.prod_1.stripped.dcd').as_posix()
+PQR = (_data_ref / '1oh0_equ.prod_1.stripped.pqr').as_posix()
 
 
 protein_residues = {
