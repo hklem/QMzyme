050b0d1c5d73b5db212147474de4eb839440f94a
Heidi Klem <heidi.klem@nist.gov>
2024-05-30 18:42:03 -0600

    fixed issue with charge balancing

--- QMzyme/truncation_utils.py
+++ QMzyme/truncation_utils.py
@@ -12,7 +12,7 @@ from QMzyme.QMzymeAtom import QMzymeAtom
 from QMzyme.RegionBuilder import RegionBuilder
 
 
-def cap_H(replace_atom, fixed_atom, bond_length=1.09):
+def cap_H(replace_atom, fixed_atom, bond_length=1.09, base_atom=None):
     """
     :param replace_atom: The Atom to be converted to hydrogen.
     :param fixed_atom: The Atom bound to replace_atom that serves as reference point for bond vector calculation.
@@ -25,8 +25,38 @@ def cap_H(replace_atom, fixed_atom, bond_length=1.09):
             'position': new_position, 
             'mass': 1.00794,
         }
-    new_atom = create_new_atom(fixed_atom, new_atom_dict) # used fixed atom because sometimes replaced atom comes from a different residue
+    if base_atom is None:
+        base_atom = replace_atom
+        if fixed_atom.resname == 'PRO' and fixed_atom.name == 'N':
+            new_atom_dict['charge'] = 0.0
+            base_atom = fixed_atom
+    new_atom = create_new_atom(base_atom, new_atom_dict) # used fixed atom because sometimes replaced atom comes from a different residue
     return new_atom
+
+def balance_charge(region, truncated_region):
+    """
+    To be used if original region has atom charge information, 
+    so the QMzymeRegion guess_charge() and read_charges() methods do not get messed up.
+    Within a residue, any atoms added should distribute the charges of any atoms removed. 
+    """
+    for res in region.residues:
+        truncated_res = truncated_region.get_residue(res.resid)
+        removed_atoms = []
+        added_atoms = []
+        chrg = 0
+        for atom in res.atoms:
+            if atom.name not in [atom.name for atom in truncated_res.atoms]:
+                removed_atoms.append(atom)
+        for atom in truncated_res.atoms:
+            if atom.name not in [atom.name for atom in res.atoms]:
+                added_atoms.append(atom)
+        if len(removed_atoms) == 0:
+            continue
+        for atom in removed_atoms:
+            chrg += atom.charge
+        fractional_charge = chrg/len(added_atoms)
+        for atom in added_atoms:
+            atom.charge = fractional_charge
 
 
 def set_bond_length(mobile_coords, fixed_coords, new_length):
@@ -44,8 +74,8 @@ def set_bond_length(mobile_coords, fixed_coords, new_length):
 #     return name
 
 
-def create_new_atom(atom, new_atom_dict):
-    for key, val in atom.__dict__.items():
+def create_new_atom(base_atom, new_atom_dict):
+    for key, val in base_atom.__dict__.items():
         if key.startswith('_QMzymeAtom__'):
             key = key.split('_QMzymeAtom__')[-1]
         if key not in new_atom_dict:
