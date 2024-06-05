6ff23cae4c9647343f29fe8ee5568ff74a4e4158
Heidi Klem <heidi.klem@nist.gov>
2024-06-05 14:11:30 -0600

    cleaned up classes especially CalculateModel

--- QMzyme/CalculateModel.py
+++ QMzyme/CalculateModel.py
@@ -22,135 +22,125 @@ class CalculateModel:
     """
     calculation = {}
     calc_type = None
-    def _add(self, type, region):
-        if type != 'QM':
-            if 'QM' not in CalculateModel.calculation:
-                raise UserWarning("You must set the high-region QM method first. "
-                                  "See QMzyme.CalculateModel.QM_Method().")
-        if CalculateModel.calc_type == None:
-            CalculateModel.calc_type = 'QM'
-            CalculateModel.calculation[type] = region
+
+    def add(self, type, region):
+        if type == 'QM' and 'QM' in CalculateModel.calculation:
+            # If region is same, we assume the user is trying to overwrite the old method
+            # Otherwise, we will rename the type to allow for multiple QM regions 
+            if region != CalculateModel.calculation['QM']:
+                count = 2
+                while type in CalculateModel.calculation:
+                    type = f'QM{count}'
+                    count += 1
+        CalculateModel.calculation[type] = region
+        CalculateModel.calc_type = type
+
+    def combine_regions_and_methods():
+        if len(CalculateModel.calculation) == 1:
             return
-        if type in CalculateModel.calculation:
-            if type == 'QM':
-                if CalculateModel.calculation['QM'] == region:
-                    if CalculateModel.calc_type != 'QM':
-                        raise UserWarning("Cannot alter QM region after another region "
-                                          "has been assigned a method. Please reset calculation "
-                                          "by running QMzyme.CalculateModel.CalculateModel._reset(), then "
-                                          "you can reassign the methods.")
-                    del CalculateModel.calculation['QM']
-                elif 'QM2' in CalculateModel.calculation:
-                    if CalculateModel.calculation['QM2'] == region:
-                        del CalculateModel.calculation['QM2']
-                        CalculateModel.calc_type = CalculateModel.calc_type.remove('QM2')
-                        type = 'QM2'
-                        region.method["type"] = 'QM2'
-                        region.set_atom_segid(region.method["type"])
-                    else:
-                        raise UserWarning("Two QM regions have already been defined."
-                                          "QMzyme currently only supports a maximum "
-                                          "of two QM methods in a single calculation.")
-                else:
-                    type = 'QM2'
-                    region.method["type"] = 'QM2'
-                    region.set_atom_segid(region.method["type"])
-            
-            elif CalculateModel.calculation[type] == region:
-                CalculateModel.calc_type = CalculateModel.calc_type.remove(type)
-                del CalculateModel.calculation[type]
-
-        # elif calc == 'QM':
-        #     if 'QM2' in CalculateModel.calculation:
-        #         if CalculateModel.calculation['QM2'] == region:
-        #             del CalculateModel.calculation['QM2']
-        #         else:
-        #             raise UserWarning("Two QM regions have already been defined."
-        #                               "QMzyme currently only supports a maximum "
-        #                               "of two QM regions in a single calculation.")
-        #     calc = 'QM2'
-        #     region.method["type"] = 'QM2'
-        #     region.set_atom_segid(region.method["type"])
-
-            # if calc in CalculateModel.calculation:
-            #     if 'QM2' in CalculateModel.calculation:
-            #         raise UserWarning("Two QM regions have already been defined."
-            #                           "QMzyme currently only supports a maximum "
-            #                           "of two QM regions in a single calculation.")
-            #     calc = 'QM2'
-            #     region.method["type"] = 'QM2'
-            #     region.set_atom_segid(region.method["type"])
-            #     #print(f"WARNING: A QM region already defined. Setting QMzymeRegion {region.name} as QM2.")
-
-        self.calc_type = type
-        self.region = region
-        CalculateModel.calculation[self.calc_type] = region
-    
-        if len(CalculateModel.calculation) > 1:
-            self.region = region
-            self._combine_regions()
-            #self._check_overlap(region)
-
-    def _combine_regions(self):
-        #region = self.region
-        base_region = CalculateModel.calculation['QM']
-        calc_type = 'QM'
-        for calc in CalculateModel.calculation:
-            if calc == 'QM':
-                continue
-            calc_type+=calc
-            #if calc_type in CalculateModel.calculation and calc_type!=self.region.method["type"]:
-            if calc_type in CalculateModel.calculation and calc_type!=self.calc_type:
-                # makes 3-scale method possible (QMQMXTB, QMQMChargeField, QMXTBChargeField)
-                base_region = CalculateModel.calculation[calc_type]
-            combined_region = base_region+self.region
-            combined_region.set_method(self.region.method)
-            combined_region.method["type"] = calc_type
-            combined_region.method["program"] = base_region.method["program"]
-            combined_region.name = f'{calc_type}'
-            #self.calc_type = calc_type
-
-        if self.region != combined_region:
-            print(f"\nCombining regions {self.region} and {base_region} to create {calc_type} region.")
-            print(f"Region stored in CalculateModel.calculation dictionary under key '{calc_type}'.")
-            # If self.region does not contain base_region, just add base_region charge and self.region charge
-            if self.region-base_region == self.region:
-                combined_region.set_charge(self.region.charge+base_region.charge)
-            else:
-                combined_region.guess_charge(verbose=False)
-                print(f"\nEstimated charge for {combined_region}, the combination of {self.region} and {base_region}, is {combined_region.charge}.")
-                print(f"\nPlease double check this value. See QMzymeRegion.set_charge() to modify this value if it is incorrect.")
-        elif self.region == combined_region:
-            combined_region.set_charge(self.region.charge)
-        fixed_ids = [atom.id for atom in base_region.get_atoms(attribute='is_fixed', value=True)]
-        fixed_ids + [atom.id for atom in self.region.get_atoms(attribute='is_fixed', value=True)]
-        combined_region.set_fixed_atoms(fixed_ids)
-        CalculateModel.calculation[calc_type] = combined_region
+        # Start with QM region(s)
+        # collect QM_regions:
+        calc_type = ''
+        qm_regions_sorted = sorted([calc for calc in CalculateModel.calculation if 'QM' in calc])
+        base_region = CalculateModel.calculation[qm_regions_sorted[0]]
+        calc_type += qm_regions_sorted[0]
+        if len(qm_regions_sorted) > 1:
+             for i in range(1,len(qm_regions_sorted)):
+                  combined = base_region.combine(CalculateModel.calculation[qm_regions_sorted[i]])
+                  base_region = combined
+                  calc_type += qm_regions_sorted[i]
+        for calc in ['XTB', 'ChargeField']:
+            if calc in CalculateModel.calculation:
+                combined = base_region.combine(CalculateModel.calculation[calc])
+                calc_type += calc
         CalculateModel.calc_type = calc_type
-        CalcMethodsRegistry._get_calc_method(calc_type)().assign_to_region(region=combined_region)
+        CalculationFactory._make_calculation(calc_type)().assign_to_region(region=combined)
+        CalculateModel.calculation[calc_type] = combined
 
-    def _check_overlap(self, region):
-        high_region = CalculateModel.calculation['QM']
-        low_region = region
+    # def _add(self, type, region):
+    #     if type != 'QM':
+    #         if 'QM' not in CalculateModel.calculation:
+    #             raise UserWarning("You must set the high-region QM method first. "
+    #                               "See QMzyme.CalculateModel.QM_Method().")
+    #     if CalculateModel.calc_type == None:
+    #         CalculateModel.calc_type = 'QM'
+    #         CalculateModel.calculation[type] = region
+    #         return
+    #     if type in CalculateModel.calculation:
+    #         if type == 'QM':
+    #             if CalculateModel.calculation['QM'] == region:
+    #                 if CalculateModel.calc_type != 'QM':
+    #                     raise UserWarning("Cannot alter QM region after another region "
+    #                                       "has been assigned a method. Please reset calculation "
+    #                                       "by running QMzyme.CalculateModel.CalculateModel._reset(), then "
+    #                                       "you can reassign the methods.")
+    #                 del CalculateModel.calculation['QM']
+    #             elif 'QM2' in CalculateModel.calculation:
+    #                 if CalculateModel.calculation['QM2'] == region:
+    #                     del CalculateModel.calculation['QM2']
+    #                     CalculateModel.calc_type = CalculateModel.calc_type.remove('QM2')
+    #                     type = 'QM2'
+    #                     region.method["type"] = 'QM2'
+    #                     region.set_atom_segid(region.method["type"])
+    #                 else:
+    #                     raise UserWarning("Two QM regions have already been defined."
+    #                                       "QMzyme currently only supports a maximum "
+    #                                       "of two QM methods in a single calculation.")
+    #             else:
+    #                 type = 'QM2'
+    #                 region.method["type"] = 'QM2'
+    #                 region.set_atom_segid(region.method["type"])
+            
+    #         elif CalculateModel.calculation[type] == region:
+    #             CalculateModel.calc_type = CalculateModel.calc_type.remove(type)
+    #             del CalculateModel.calculation[type]
 
-        high_region.segid = 'QM'
-        common_atoms = high_region.get_overlap(low_region)
-        if len(common_atoms) != 0:
-            #residues = [atom.resname+str(atom.resid) for atom in common_atoms]
-            residues = [atom.resid for atom in common_atoms]
-            residues = [high_region.get_residue(resid) for resid in list(set(residues))]
-            subtracted = low_region.subtract(high_region)
-            subtracted.guess_charge(verbose=False)
-            subtracted.method = low_region.method
-            subtracted.method["charge"] = subtracted.charge
-            subtracted.method["freeze_atoms"] = subtracted.get_indices(attribute='is_fixed', value=True)
-            subtracted.name = low_region.name
-            print(f"\nWARNING: Region overlap detected. The following residue(s) were found in both regions: {list(set(residues))}.")
-            print(f"Removing duplicate atoms and recalculating charge for calculation.")
-            print(f"Subtracted region has a charge of {subtracted.charge}. Both regions combine for a total charge of {subtracted.charge+high_region.charge}.")
-            print(f"The original region {low_region.name} remains unchanged in the GenerateModel object 'regions' attribute.")
-                        
-            CalculateModel.calculation[low_region.method["type"]] = subtracted
+    #     self.calc_type = type
+    #     self.region = region
+    #     CalculateModel.calculation[self.calc_type] = region
+    
+    #     if len(CalculateModel.calculation) > 1:
+    #         self.region = region
+    #         self._combine_regions()
+    #         #self._check_overlap(region)
+
+    # def _combine_regions(self):
+    #     #region = self.region
+    #     base_region = CalculateModel.calculation['QM']
+    #     calc_type = 'QM'
+    #     for calc in CalculateModel.calculation:
+    #         if calc == 'QM':
+    #             continue
+    #         calc_type+=calc
+    #         #if calc_type in CalculateModel.calculation and calc_type!=self.region.method["type"]:
+    #         if calc_type in CalculateModel.calculation and calc_type!=self.calc_type:
+    #             # makes 3-scale method possible (QMQMXTB, QMQMChargeField, QMXTBChargeField)
+    #             base_region = CalculateModel.calculation[calc_type]
+    #         combined_region = base_region+self.region
+    #         combined_region.set_method(self.region.method)
+    #         combined_region.method["type"] = calc_type
+    #         combined_region.method["program"] = base_region.method["program"]
+    #         combined_region.name = f'{calc_type}'
+    #         #self.calc_type = calc_type
+
+    #     if self.region != combined_region:
+    #         print(f"\nCombining regions {self.region} and {base_region} to create {calc_type} region.")
+    #         print(f"Region stored in CalculateModel.calculation dictionary under key '{calc_type}'.")
+    #         # If self.region does not contain base_region, just add base_region charge and self.region charge
+    #         if self.region-base_region == self.region:
+    #             combined_region.set_charge(self.region.charge+base_region.charge)
+    #         else:
+    #             combined_region.guess_charge(verbose=False)
+    #             print(f"\nEstimated charge for {combined_region}, the combination of {self.region} and {base_region}, is {combined_region.charge}.")
+    #             print(f"\nPlease double check this value. See QMzymeRegion.set_charge() to modify this value if it is incorrect.")
+    #     elif self.region == combined_region:
+    #         combined_region.set_charge(self.region.charge)
+    #     fixed_ids = [atom.id for atom in base_region.get_atoms(attribute='is_fixed', value=True)]
+    #     fixed_ids + [atom.id for atom in self.region.get_atoms(attribute='is_fixed', value=True)]
+    #     combined_region.set_fixed_atoms(fixed_ids)
+    #     CalculateModel.calculation[calc_type] = combined_region
+    #     CalculateModel.calc_type = calc_type
+    #     CalculationFactory._make_calculation(calc_type)().assign_to_region(region=combined_region)
 
     def _reset():
         CalculateModel.calculation = {}
@@ -163,7 +153,7 @@ class CalculationBase:
         region.set_method(self.__dict__)
         self._set_charge(region, charge)
         region.set_atom_segid(region.method["type"])
-        CalculateModel()._add(type=self.type, region=region)
+        CalculateModel().add(type=self.type, region=region)
         #region.set_atom_segid(region.method["type"])
         #self._set_charge(CalculateModel.calculation[self.type], charge)
 
@@ -236,8 +226,10 @@ class MultiscaleCalculationBase(CalculationBase):
         self.region = region
         self._set_constraints(region)
         self.mult = mult
+        region.set_method(self.__dict__)
+        region.method["program"] = 'orca'
         #self._set_charge(CalculateModel.calculation[self.type], charge)
-        #self._set_charge(self.region, charge)
+        self._set_charge(self.region, charge)
         self._set_qm_input()
         # fix segids
         for atom in self.region.atoms:
@@ -247,10 +239,9 @@ class MultiscaleCalculationBase(CalculationBase):
                 #setattr(atom, "segid", CalculateModel.calc_type[2:])
                 setattr(atom, "segid", CalculateModel.calc_type[2:])
 
-
     def _set_qm_input(self):
         # QM/QM2 or QM/XTB. QMChargeField duck types this method.
-        qm_input = f'QM/{self.type[-3:]}' 
+        qm_input = f"QM/{CalculateModel.calc_type.replace('QM', '', 1)}" 
         if qm_input not in CalculateModel.calculation['QM'].method['qm_input']:
             qm_input += f" {CalculateModel.calculation['QM'].method['qm_input']}"
         qmmm_section = f"%QMMM {self.qm_input}\n"
@@ -278,7 +269,6 @@ class MultiscaleCalculationBase(CalculationBase):
                     range += f":{i}"+"} "
         return range.strip()
 
-
 class _QMQM2_Method(MultiscaleCalculationBase):
     def __init__(self):
         self.type = 'QMQM2'
@@ -298,24 +288,28 @@ class _QMChargeField_Method(MultiscaleCalculationBase):
         self.type = 'QMChargeField'
         self.qm_input = ''
 
-
-class CalcMethodsRegistry:
+class CalculationFactory:
     """
     Factory Class to Register Concrete Calculation Method Classes.
     """
     calc_methods = {}
-    def _register_method(calc_type, class_name):
-        CalcMethodsRegistry.calc_methods[calc_type] = class_name
+    def _register_method(calculation_type, class_name):
+        CalculationFactory.calc_methods[calculation_type] = class_name
 
-    def _get_calc_method(calc_type):
-        calc_method = CalcMethodsRegistry.calc_methods.get(calc_type)
-        if not calc_method:
-            raise UserWarning(f"Calculation method {calc_method} not found.")
-        return calc_method
-    
-CalcMethodsRegistry._register_method('QM', QM_Method)
-CalcMethodsRegistry._register_method('XTB', XTB_Method)
-# CalcMethodsRegistry._register_method('ChargeField', ChargeField_Method)
-CalcMethodsRegistry._register_method('QMQM2', _QMQM2_Method)
-CalcMethodsRegistry._register_method('QMXTB', _QMXTB_Method)
-# CalcMethodsRegistry._register_method('QMChargeField', _QMChargeField_Method)
+    def _make_calculation(calculation_type):
+        calculation = CalculationFactory.calc_methods.get(calculation_type)
+        if not calculation:
+            raise UserWarning(f"Calculation method {calculation} not found.")
+        return calculation
+
+possible_methods = {
+     'QM': QM_Method,
+     'XTB': XTB_Method,
+     'ChargeField': ChargeField_Method,
+     'QMQM2':_QMQM2_Method,
+     'QMXTB': _QMXTB_Method,
+     'QMChargeField': _QMChargeField_Method
+}
+for key, val in possible_methods.items():
+    CalculationFactory._register_method(key, val)
+     
