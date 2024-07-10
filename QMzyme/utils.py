74954c6614987752abcd71c1861ea89d01da2c6b
Heidi Klem <heidi.klem@nist.gov>
2024-07-10 11:50:15 -0600

    added rmsd and related functions

--- QMzyme/utils.py
+++ QMzyme/utils.py
@@ -7,13 +7,6 @@
 Module containing broad functionality utilized throughout the package.
 """
 
-def check_filename(filename, format):
-    if filename.endswith(format):
-        return filename
-    if not format.startswith('.'):
-            format = '.'+format
-    return filename.split('.')[0]+format
-
 from functools import singledispatch
 from QMzyme.QMzymeModel import QMzymeModel
 from QMzyme.RegionBuilder import RegionBuilder
@@ -23,7 +16,14 @@ from QMzyme.SelectionSchemes import SelectionScheme
 from QMzyme.SelectionSchemes import DistanceCutoff
 from abc import ABCMeta
 from QMzyme.QMzymeRegion import QMzymeRegion
+import numpy as np
 
+def check_filename(filename, format):
+    if filename.endswith(format):
+        return filename
+    if not format.startswith('.'):
+            format = '.'+format
+    return filename.split('.')[0]+format
 
 @singledispatch
 def make_selection(selection, model: QMzymeModel, name=None, **kwargs):
@@ -62,3 +62,38 @@ def scheme_selection(selection: ABCMeta, model: QMzymeModel, name, **kwargs):
     s = selection(model=model, name=name, **kwargs)
     region = s.return_region()
     return region
+
+def rmsd(xyz1, xyz2, align=False):
+    if align == True:
+        t, r = compute_translation_and_rotation(xyz1, xyz2)
+        xyz1 = kabsch_transform(xyz1, t, r)
+    delta = xyz1 - xyz2
+    rmsd = (delta ** 2.0).sum(1).mean() ** 0.5
+    return rmsd
+
+def compute_translation_and_rotation(mobile, target):
+    #meta data
+    n_atoms = mobile.shape[0]
+    n_dim = mobile.shape[1]
+    mu1 = np.zeros(n_dim)
+    mu2 = np.zeros(n_dim)
+    for i in range(n_atoms):
+        for j in range(n_dim):
+            mu1[j] += mobile[i,j]
+            mu2[j] += target[i,j]
+    mu1 /= n_atoms
+    mu2 /= n_atoms
+    mobile = mobile - mu1
+    target = target - mu2
+
+    correlation_matrix = np.dot(np.transpose(mobile), target)
+    V, S, W_tr = np.linalg.svd(correlation_matrix)
+    if np.linalg.det(V) * np.linalg.det(W_tr) < 0.0:
+        V[:, -1] = -V[:, -1]
+    rotation = np.dot(V, W_tr)
+    translation = mu2 - np.dot(mu1,rotation)
+    return translation, rotation
+
+def kabsch_transform(mobile, translation, rotation):
+    mobile_prime = np.dot(mobile,rotation) + translation
+    return mobile_prime
